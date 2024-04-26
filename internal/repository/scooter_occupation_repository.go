f33210aae08cfd27038a7d347f35bc9b9252f918
boolfixer <pavel.lysiankou.96@gmail.com>
2024-04-26 22:02:15 +0200

    update scooter occupation repository skeleton

--- internal/repository/scooter_occupation_repository.go
+++ internal/repository/scooter_occupation_repository.go
@@ -3,22 +3,23 @@ package repository
 import (
 	"database/sql"
 	"github.com/google/uuid"
+	"time"
 )
 
 type ScooterOccupationRepository interface {
-	Create(scooterUuid uuid.UUID, userUuid uuid.UUID)
-	SetReleasedAtByScooterUuidAndUserUuid(scooterUuid uuid.UUID, userUuid uuid.UUID)
+	Create(scooterUuid uuid.UUID, userUuid uuid.UUID, occupiedAt time.Time)
+	SetReleasedAtByScooterUuidAndUserUuid(releasedAt time.Time, scooterUuid uuid.UUID, userUuid uuid.UUID)
 }
 
 type mysqlScooterOccupationRepository struct {
 	db *sql.DB
 }
 
-func (r mysqlScooterOccupationRepository) Create(scooterUuid uuid.UUID, userUuid uuid.UUID) {
+func (r mysqlScooterOccupationRepository) Create(scooterUuid uuid.UUID, userUuid uuid.UUID, occupiedAt time.Time) {
 	return
 }
 
-func (r mysqlScooterOccupationRepository) SetReleasedAtByScooterUuidAndUserUuid(scooterUuid uuid.UUID, userUuid uuid.UUID) {
+func (r mysqlScooterOccupationRepository) SetReleasedAtByScooterUuidAndUserUuid(releasedAt time.Time, scooterUuid uuid.UUID, userUuid uuid.UUID) {
 	return
 }
 
