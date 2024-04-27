7b93ed1424d95fe334d9d1f8ed6aadb8f5616108
boolfixer <pavel.lysiankou.96@gmail.com>
2024-04-27 10:48:51 +0200

    fix migration that creates scooters_occupation table

---
+++ migration/20240425201445_create_table_scooters_occupations_table.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE scooters_occupations;
--- migration/20240425201445_create_table_scooters_occupations_table.up.sql
+++ migration/20240425201445_create_table_scooters_occupations_table.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE scooters_occupations(
     id BINARY(16) NOT NULL,
     scooter_id BINARY(16) NOT NULL,
     user_id BINARY(16) NOT NULL,
-    occupied_at DATETime NOT NULL,
-    released_at DATETime NOT NULL,
+    occupied_at DATETIME NOT NULL,
+    released_at DATETIME DEFAULT NULL,
     PRIMARY KEY(id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
