ad4cc08c510f137eeb86693cdac2584ba34f6e84
boolfixer <pavel.lysiankou.96@gmail.com>
2024-04-30 17:17:40 +0200

    install GoMock, create mock for scooter repo, add test for search scooters handler

---
+++ internal/scooter_handler/search_scooters_handler_test.go
@@ -0,0 +1,39 @@
+package scooter_handler
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"main/internal/dto"
+	mock_repository "main/internal/mock/repository"
+	"main/internal/model"
+	"testing"
+)
+
+func TestSearchScootersHandler(t *testing.T) {
+	location := dto.Location{Latitude: 15, Longitude: 13}
+
+	latitudeStart := 12
+	longitudeStart := 10
+	latitudeEnd := 18
+	longitudeEnd := 16
+
+	scooter := model.Scooter{Id: uuid.UUID{}, Name: "Test scooter", Latitude: 14, Longitude: 16}
+
+	ctrl := gomock.NewController(t)
+	scooterRepository := mock_repository.NewMockScooterRepository(ctrl)
+	scooterRepository.
+		EXPECT().
+		FindScootersByArea(latitudeStart, longitudeStart, latitudeEnd, longitudeEnd).
+		Return([]model.Scooter{scooter})
+
+	handler := SearchScootersHandler{scooterRepository: scooterRepository}
+	scooters := handler.Handle(location)
+
+	if len(scooters) != 1 {
+		t.Errorf("failed to assert scooters count; got %d, wanted %d", len(scooters), 1)
+	}
+
+	if scooter != scooters[0] {
+		t.Errorf("unexpected scooter record returned")
+	}
+}
