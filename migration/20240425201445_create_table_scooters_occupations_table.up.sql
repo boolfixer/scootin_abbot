245872f82a33b7f3617aab8bed2dc07f62f15115
boolfixer <pavel.lysiankou.96@gmail.com>
2024-04-30 09:26:13 +0200

    change unique index based one column in scooters occupations table

--- migration/20240425201445_create_table_scooters_occupations_table.up.sql
+++ migration/20240425201445_create_table_scooters_occupations_table.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE scooters_occupations(
     scooter_id BINARY(16) NOT NULL,
     user_id BINARY(16) NOT NULL,
     PRIMARY KEY(id),
-    UNIQUE KEY unique_scooter_id_user_id(scooter_id, user_id)
+    UNIQUE KEY unique_scooter_id(scooter_id)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB;
