78e1531735c1be788be809b47d7484faa73a9cdf
boolfixer <pavel.lysiankou.96@gmail.com>
2024-04-30 09:29:19 +0200

    add custom error processing in error middleware

--- internal/middleware/error_middleware.go
+++ internal/middleware/error_middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"main/internal/http_error"
 	"net/http"
 )
 
@@ -15,8 +16,13 @@ func (m ErrorMiddleware) Handle() gin.HandlerFunc {
 
 		for _, err := range c.Errors {
 			switch e := err.Err.(type) {
+
 			case validator.ValidationErrors:
 				c.AbortWithStatusJSON(http.StatusBadRequest, formatValidationErrors(e))
+			case http_error.NotFoundError:
+				c.AbortWithStatusJSON(http.StatusNotFound, map[string]string{"message": e.Error()})
+			case http_error.ConflictError:
+				c.AbortWithStatusJSON(http.StatusConflict, map[string]string{"message": e.Error()})
 			default:
 				c.AbortWithStatusJSON(http.StatusInternalServerError, map[string]string{"message": "Server error."})
 			}
